cd8ff6d63df179dbe2f7b25b5412b7d1eae135c8
Shohidur Rahman <sohidurr49@gmail.com>
2018-07-12 15:12:41 +0600

    some changes

--- app/Http/Controllers/taskController/MrfListController.php
+++ app/Http/Controllers/taskController/MrfListController.php
@@ -2,13 +2,26 @@
 
 namespace App\Http\Controllers\taskController;
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\dataget\ListGetController;
+use App\Http\Controllers\Message\StatusMessage;
 use App\Http\Controllers\Controller;
+use App\Http\Controllers\RoleManagement;
+use Illuminate\Http\Request;
+use App\Model\MxpBookingChallan;
+use App\Model\MxpMrf;
+use Carbon\Carbon;
+use Validator;
+use Auth;
+use DB;
 
 class MrfListController extends Controller
 {
     public function mrfListView(){
-    	$bookingList =[];
+    	$bookingList = DB::table('mxp_MRF_table')
+    						->where('user_id',Auth::user()->user_id)
+    						->groupBy('mrf_id')
+                      		->orderBy('id','DESC')
+                      		->paginate(15);
     	return view('maxim.mrf.list.mrfList',compact('bookingList'));
     }
 }
