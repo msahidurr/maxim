b9cd741bfb9fc20662743133c4d9022993904ca2
tapumandal <online.tapu@gmail.com>
2018-07-13 18:31:33 +0600

    ascdad

---
+++ app/MxpProductsColors.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class MxpProductsColors extends Model
+{
+    //
+//    protected $primaryKey = 'id';
+//    protected $fillable = ['product_id', 'color_id', 'status'];
+}
