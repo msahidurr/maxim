be5f77d90df29b558b4d4eb7525eb0464fa88c91
Shohidur Rahman <sohidurr49@gmail.com>
2018-07-13 18:08:53 +0600

    dfdf

--- resources/views/maxim/layouts/layouts.blade.php
+++ resources/views/maxim/layouts/layouts.blade.php
@@ -2,7 +2,10 @@
 <!-- <html lang="en" class="no-js"> -->
 <head>
 	<meta charset="utf-8"/>
-	<title>Maxim</title>
+	<!-- <title>Maxim</title> -->
+	<title>
+		@yield('title')
+	</title>
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta content="width=device-width, initial-scale=1" name="viewport"/>
 	<meta content="" name="description"/>
