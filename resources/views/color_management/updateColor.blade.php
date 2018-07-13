2c78d8b1d1b38e981eee39e732da93ec80df896e
tapumandal <online.tapu@gmail.com>
2018-07-13 18:36:56 +0600

    afcsadsd

--- resources/views/color_management/updateColor.blade.php
+++ resources/views/color_management/updateColor.blade.php
@@ -20,60 +20,54 @@
 					<div class="panel-heading">{{trans('others.update_color_label')}}</div>
 					@foreach ($MxpGmtsColor as $GmtsColor)
 					<div class="panel-body">
-			            
+
 						<form class="form-horizontal" role="form" method="POST" action="{{ Route('update_gmtscolor_action') }}/{{$GmtsColor->id}}">
-							<input type="hidden" name="_token" value="{{ csrf_token() }}">							
+							<input type="hidden" name="_token" value="{{ csrf_token() }}">
 
 						<div class="col-md-12">
-							
 
-							<div class="form-group col-md-6">
-								<label class="col-md-4 control-label">{{trans('others.product_code_label')}}</label>
-								<div class="col-md-8">
-									<select class="form-control" type="select" name="p_code" >
-										<option value="{{$GmtsColor->item_code}}">{{$GmtsColor->item_code}}</option>
 
-										@foreach($itemCodes as $itemCode)		
-										<option value="{{$itemCode->product_code}}">{{$itemCode->product_code}}</option>
-										@endforeach
+							{{--<div class="form-group col-md-6">--}}
+								{{--<label class="col-md-4 control-label">{{trans('others.product_code_label')}}</label>--}}
+								{{--<div class="col-md-8">--}}
+									{{--<select class="form-control" type="select" name="p_code" >--}}
+										{{--<option value="{{$GmtsColor->item_code}}">{{$GmtsColor->item_code}}</option>--}}
 
-									</select>
-								</div>
-							</div>	
+										{{--@foreach($itemCodes as $itemCode)--}}
+										{{--<option value="{{$itemCode->product_code}}">{{$itemCode->product_code}}</option>--}}
+										{{--@endforeach--}}
+
+									{{--</select>--}}
+								{{--</div>--}}
+							{{--</div>--}}
 
 							<div class="form-group col-md-6">
 								<label class="col-md-4 control-label">{{trans('others.gmts_color_label')}}</label>
 								<div class="col-md-8">
+									<input type="hidden" class="form-control" name="color_id" value="{{ $GmtsColor->id  }}">
 									<input type="text" class="form-control" name="gmts_color" value="{{ $GmtsColor->color_name  }}">
 								</div>
 							</div>
-						</div>
-						
-
-						<div class="col-md-12">
-							<div class="form-group">
-								<div class="col-md-3 col-md-offset-5">
-									<div class="select">
-										<select class="form-control" type="select" name="isActive" >
-											<option value="{{$GmtsColor->status}}">
-                                                {{($GmtsColor->status == 1) ? "Active" : "Inactive"}}
-                                            </option>
-											<option  value="1" name="isActive" >{{ trans("others.action_active_label") }}</option>
-											<option value="0" name="isActive" >{{ trans("others.action_inactive_label") }}</option>
-									    </select>
-									</div>
+							<div class="col-md-3">
+								<div class="select">
+									<select class="form-control" type="select" name="isActive" >
+										<option value="{{$GmtsColor->status}}">
+											{{($GmtsColor->status == 1) ? "Active" : "Inactive"}}
+										</option>
+										<option  value="1" name="isActive" >{{ trans("others.action_active_label") }}</option>
+										<option value="0" name="isActive" >{{ trans("others.action_inactive_label") }}</option>
+									</select>
 								</div>
 							</div>
-							
-							<div class="form-group">
-								<div class="col-md-6 col-md-offset-5">
-									<button type="submit" class="btn btn-primary" style="margin-right: 15px;">
-										{{trans('others.update_button')}}
-									</button>
-								</div>
+							<div class="col-md-3">
+								<button type="submit" class="btn btn-primary" style="margin-right: 15px;">
+									{{trans('others.update_button')}}
+								</button>
 							</div>
-
 						</div>
+
+
+
 						</form>
 					</div>
 					@endforeach
